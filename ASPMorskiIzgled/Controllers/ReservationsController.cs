7340e97f49efff9b9130f2c9835e21103a985285
MartoP7 <martin.papazov489@edu1.pgzaimov.com>
2026-04-03 01:02:59 +0300

    update

---
+++ ASPMorskiIzgled/Controllers/ReservationsController.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ASPMorskiIzgled.Data;
+using ASPMorskiIzgled.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ASPMorskiIzgled.Controllers
+{
+    [Authorize]
+    public class ReservationsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<Client> _userManager;
+
+        public ReservationsController(ApplicationDbContext context, UserManager<Client> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Reservations
+        public async Task<IActionResult> Index()
+        {
+            if (User.IsInRole("Admin"))
+            {
+                var applicationDbContext = _context.Reservations.Include(r => r.Clients).Include(r => r.Rooms);
+                return View(await applicationDbContext.ToListAsync());
+            }
+            else
+            {
+                var applicationDbContext = _context.Reservations.
+                    Include(r => r.Clients).
+                    Include(r => r.Rooms).Where(r => r.ClientId == _userManager.GetUserId(User));
+                return View(await applicationDbContext.ToListAsync());
+            }
+        }
+
+        // GET: Reservations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservations
+                .Include(r => r.Clients)
+                .Include(r => r.Rooms)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            return View(reservation);
+        }
+
+        // GET: Reservations/Create
+        public IActionResult Create()
+        {
+            //ViewData["ClientId"] = new SelectList(_context.Users, "Id", "Id");
+            ViewData["RoomId"] = new SelectList(_context.Rooms, "Id", "Description");
+            
+            return View();
+        }
+
+
+        public async Task<IActionResult> CreateReservation(int roomId, DateTime dateIn, DateTime dateOut)
+        {
+            Reservation reservation = new Reservation
+            {
+                ReservationDate = DateTime.Now,
+                ClientId = _userManager.GetUserId(User),
+                RoomId = roomId,
+                DateIn = dateIn,
+                DateOut = dateOut
+            };
+            _context.Reservations.Add(reservation);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        // POST: Reservations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RoomId,DateIn,DateOut")] Reservation reservation, int RoomsId)
+        {
+            reservation.ReservationDate = DateTime.Now;
+            reservation.ClientId = _userManager.GetUserId(User);
+
+            if (ModelState.IsValid)
+            {
+                _context.Reservations.Add(reservation);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            //ViewData["ClientId"] = new SelectList(_context.Users, "Id", "Id", reservation.ClientId);
+            ViewData["RoomId"] = new SelectList(_context.Rooms, "Id", "Description");
+            return View(reservation);
+        }
+
+        // GET: Reservations/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservations.FindAsync(id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+            //ViewData["ClientId"] = new SelectList(_context.Users, "Id", "Id", reservation.ClientId);
+            ViewData["RoomId"] = new SelectList(_context.Rooms, "Id", "Description", reservation.RoomId);
+            return View(reservation);
+        }
+
+        // POST: Reservations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,RoomId,DateIn,DateOut")] Reservation reservation)
+        {
+            reservation.ReservationDate = DateTime.Now;
+            reservation.ClientId = _userManager.GetUserId(User);
+
+            if (id != reservation.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(reservation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReservationExists(reservation.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            //ViewData["ClientId"] = new SelectList(_context.Users, "Id", "Id", reservation.ClientId);
+            ViewData["RoomId"] = new SelectList(_context.Rooms, "Id", "Description", reservation.RoomId);
+            return View(reservation);
+        }
+
+        // GET: Reservations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservations
+                .Include(r => r.Clients)
+                .Include(r => r.Rooms)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            return View(reservation);
+        }
+
+        // POST: Reservations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var reservation = await _context.Reservations.FindAsync(id);
+            if (reservation != null)
+            {
+                _context.Reservations.Remove(reservation);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReservationExists(int id)
+        {
+            return _context.Reservations.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ASPMorskiIzgled/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ASPMorskiIzgled.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPMorskiIzgled.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<Client>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        //public DbSet<Client> Clients { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomType> RoomTypes { get; set; }
+    }
+}
