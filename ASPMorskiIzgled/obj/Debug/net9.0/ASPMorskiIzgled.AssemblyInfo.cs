691cdc88c8b07e8717a859dd0f1cae5337f15256
MartoP7 <martin.papazov489@edu1.pgzaimov.com>
2026-05-12 01:01:17 +0300

    update 1.4

--- ASPMorskiIzgled/obj/Debug/net9.0/ASPMorskiIzgled.AssemblyInfo.cs
+++ ASPMorskiIzgled/obj/Debug/net9.0/ASPMorskiIzgled.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASPMorskiIzgled")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+537d7fe6ba8c378bd8e3b764c00af2e4167d4f9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASPMorskiIzgled")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASPMorskiIzgled")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
